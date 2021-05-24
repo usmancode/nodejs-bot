15c2606e7bd6bedfcadab697746d2c6fb9234127
usman khan <usmanhbtu@gmail.com>
2021-05-24 23:07:13 +0400

    initial

---
+++ bot/bot.js
@@ -0,0 +1,77 @@
+const ethers = require('ethers');
+
+const addresses = {
+  WETH: '0xc778417e063141139fce010982780140aa0cd5ab',
+  factory: '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f', 
+  router: '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D',
+  recipient: '0xC29cB80eF9A***********************' // usman metamask address
+}
+
+const mnemonic = 'tell ******** ********* ****** awkward foil horror';
+
+const provider = new ethers.providers.WebSocketProvider('wss://rinkeby.infura.io/ws/v3/95193c2ce15a45**************');
+const wallet = ethers.Wallet.fromMnemonic(mnemonic);
+const account = wallet.connect(provider);
+const factory = new ethers.Contract(
+  addresses.factory,
+  ['event PairCreated(address indexed token0, address indexed token1, address pair, uint)'],
+  account
+);
+
+const router = new ethers.Contract(
+  addresses.router,
+  [
+    'function getAmountsOut(uint amountIn, address[] memory path) public view returns (uint[] memory amounts)',
+    'function swapExactTokensForTokens(uint amountIn, uint amountOutMin, address[] calldata path, address to, uint deadline) external returns (uint[] memory amounts)'
+  ],
+  account
+);
+//console.log(account)
+factory.on('PairCreated', async (token0, token1, pairAddress) => {
+  console.log(`
+    New pair detected
+    =================
+    token0: ${token0}
+    token1: ${token1}
+    pairAddress: ${pairAddress}
+  `);
+
+  //The quote currency needs to be WETH (we will pay with WETH)
+  let tokenIn, tokenOut;
+  if(token0 === addresses.WETH) {
+    tokenIn = token0; 
+    tokenOut = token1;
+  }
+
+  if(token1 == addresses.WETH) {
+    tokenIn = token1; 
+    tokenOut = token0;
+  }
+
+  //The quote currency is not WETH
+  if(typeof tokenIn === 'undefined') {
+    return;
+  }
+
+  //We buy for 0.1 ETH of the new token
+  const amountIn = ethers.utils.parseUnits('0.1', 'ether');
+  const amounts = await router.getAmountsOut(amountIn, [tokenIn, tokenOut]);
+  //Our execution price will be a bit different, we need some flexbility
+  const amountOutMin = amounts[1].sub(amounts[1].div(10));
+  console.log(`
+    Buying new token
+    =================
+    tokenIn: ${amountIn.toString()} ${tokenIn} (WETH)
+    tokenOut: ${amounOutMin.toString()} ${tokenOut}
+  `);
+  const tx = await router.swapExactTokensForTokens(
+    amountIn,
+    amountOutMin,
+    [tokenIn, tokenOut],
+    addresses.recipient,
+    Date.now() + 1000 * 60 * 10 //10 minutes
+  );
+  const receipt = await tx.wait(); 
+  console.log('Transaction receipt');
+  console.log(receipt);
+});
